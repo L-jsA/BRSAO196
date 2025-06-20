4fa7b6b5d05eadb0287c77176fe0d5438be411be
Luana <luanajesus1935@hotmail.com>
2025-06-20 15:55:39 -0300

    Remover todas as pastas

--- LAB112/LAB112.py
+++
@@ -1,7 +0,0 @@
-#LIÇÃO SETE - CATEGORIZAR VALORES
-
-listaMista = [45, 290578, 1.02, True, "Meu cachorro está na cama.", "45"] # lista com vários tipos de dados
-
-for list in listaMista: # percorre a lista
-    print("{} é do tipo de dado {}".format(list, type(list))) # imprime cada lista, o dado, e seu tipo.
-                                                              # .format está formatando as chaves com o elemento e valor correto 
--- LAB115/LAB115LOOP.py
+++
@@ -1,4 +0,0 @@
-#LIÇÃO DEZ - LOOPS FOR W WHILE
-print ("Contador de número: 1 a 10")
-for i  in range (1, 11): # faz uma contagem de 1 a 10
-    print (i) # imprime todos os valores
